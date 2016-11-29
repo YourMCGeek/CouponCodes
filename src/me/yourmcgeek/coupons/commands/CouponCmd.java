a246ea696d66f9e12ebe319888b56bae269a648d
YourMCGeek <yourmcgeek@icloud.com>
2016-11-28 19:54:56 -0500

    Worked on CouponCmd
    Countined work on CouponCmd

--- src/me/yourmcgeek/coupons/commands/CouponCmd.java
+++ src/me/yourmcgeek/coupons/commands/CouponCmd.java
@@ -1,5 +1,6 @@
 package me.yourmcgeek.coupons.commands;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -22,17 +23,27 @@ public class CouponCmd implements CommandExecutor {
     	if (args.length >= 1){
     		if (args[0].equalsIgnoreCase("create")){
     			// TODO: Command logic	
+    			// /coupon create CODEHERE 32 DIAMOND 
     			
     			if (args[1].isEmpty()) {
     				sender.sendMessage("§cUnkown command argument, §4" + args[1] + " please try again.");
     			}
     			
-    			if (args[1].matches("\\d*")&& args[1].length() > 2) {
-    				if (args[2].isEmpty()) {
+    			
+    			
+    			else if (args[2].matches("\\d*")&& args[1].length() > 2) {
+    				if (args[3].isEmpty()) {
     					sender.sendMessage("§cPlease enter an item.");
     				}
     				else {
-    					
+    					Material item = Material.getMaterial(args[3].toUpperCase());
+    					if (item == null) {
+    						sender.sendMessage("§cPlease provide a valid material name. §4" + args[3] + "§c is not a valid material name"
+    								+ "please try again.");
+    					}
+    					else {
+    						
+    					}
     				}
     			}
     		}
