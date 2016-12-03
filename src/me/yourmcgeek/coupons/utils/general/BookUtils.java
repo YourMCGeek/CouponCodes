05d767f1761103c7764e39a9941e23239fc453a6
YourMCGeek <yourmcgeek@icloud.com>
2016-12-02 20:02:05 -0500

    Finished book!

--- src/me/yourmcgeek/coupons/utils/general/BookUtils.java
+++ src/me/yourmcgeek/coupons/utils/general/BookUtils.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.bukkit.ChatColor.*;
+
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
@@ -46,13 +47,24 @@ public class BookUtils {
 			+ DARK_AQUA + "An example would be   " + DARK_GREEN 
 			+ "/coupons create CBE dirt|64",
 			
-			"THIS IS PAGE 2",
+			DARK_BLUE + "To view the current list of coupons, you can try running the command\n\n" + DARK_AQUA + "/coupons list"
+			 + DARK_BLUE + "\n\nType it in chat to see all your coupons!" ,
 			
-			"THIS IS PAGE 3",
+			DARK_PURPLE + "To redeem a coupon, run the command\n\n " + LIGHT_PURPLE + "/coupon redeem {code}\n\n" + 
+			DARK_PURPLE + "Lets redeem our coupon by doing the command\n\n" + LIGHT_PURPLE + "/coupons redeem CBE",
 			
-			"THIS IS PAGE 4",
+			DARK_BLUE + "Since we no longer need our coupon, lets delete it. To delete a coupon, run the command " +
+			BLUE + "/coupons delete {code}" + DARK_BLUE + ".\n\n" + "To delete our coupon lets run\n\n" + 
+					BLUE + "/coupons delete CBE",
 			
-			"THIS IS PAGE 5"
+			DARK_RED + "Other Coupon commands include\n\n" + RED + "/coupons help" + "\n\n/coupons book\n\n" +
+			DARK_RED + "Make sure to check " + RED + "/coupons help" + DARK_RED + 
+			" as that will always be updated with current commands.",
+			
+			"\n\n" + GOLD + MAGIC + "1234567891234567890" + "\n\n" + 
+			DARK_GRAY + "   Happy Discounting!\n   ~YourMCGeek and \n      2008Choco!! <3" +
+					"\n\n" + GOLD + MAGIC + "1234567891234567890"
+			
 		);
 		
 		book.setItemMeta(bookMeta);
